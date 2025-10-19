821ead77d81592ec2ac3034c5200ed4e9dd805f8
ehddud8339 <ehddud8339@gmail.com>
2025-10-19 14:09:37 +0900

    add rfuse results files

--- rfuse/driver.sh
+++ rfuse/driver.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 python3 make_xlsx.py ./ext4 --out ext4_summary.xlsx
 python3 make_xlsx.py ./fuse --out fuse_summary.xlsx
 python3 make_xlsx.py ./rfuse --out rfuse_summary.xlsx
+python3 make_xlsx.py ./rfuse_busy --out rfuse_busy_summary.xlsx
 
 python3 merge_xlsx.py . --out merged_summary.xlsx
