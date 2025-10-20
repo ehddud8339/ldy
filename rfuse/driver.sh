339c9293fb5ae94f2b0624412270292fbdaa7ed8
ehddud8339 <ehddud8339@gmail.com>
2025-10-20 14:30:51 +0900

    update from local

--- rfuse/driver.sh
+++ rfuse/driver.sh
@@ -8,5 +8,6 @@ python3 make_xlsx.py ./rfuse --out rfuse_summary.xlsx
 python3 make_xlsx.py ./rfuse_rr --out rfuse_rr_summary.xlsx
 python3 make_xlsx.py ./rfuse_thr --out rfuse_thr_summary.xlsx
 python3 make_xlsx.py ./rfuse_busy --out rfuse_busy_summary.xlsx
+python3 make_xlsx.py ./rfuse_Ecore --out rfuse_Ecore_summary.xlsx
 
 python3 merge_xlsx.py . --out merged_summary.xlsx
