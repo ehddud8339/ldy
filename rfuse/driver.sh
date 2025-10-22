fb7aaf1a225dba52064f46da681a21535697549a
ehddud8339 <ehddud8339@gmail.com>
2025-10-22 10:14:17 +0000

    update from server

--- rfuse/driver.sh
+++ rfuse/driver.sh
@@ -12,11 +12,11 @@ set -euo pipefail
 #python3 make_xlsx.py ./rfuse_busy_10 --out rfuse_busy_10_summary.xlsx
 #python3 make_xlsx.py ./rfuse_busy_20 --out rfuse_busy_20_summary.xlsx
 #python3 make_xlsx.py ./rfuse_busy_40 --out rfuse_busy_40_summary.xlsx
-python3 make_xlsx.py ./fuse --out rfuse_summary.xlsx
-python3 make_xlsx.py ./fuse_busy_5 --out rfuse_busy_5_summary.xlsx
-python3 make_xlsx.py ./fuse_busy_10 --out rfuse_busy_10_summary.xlsx
-python3 make_xlsx.py ./fuse_busy_20 --out rfuse_busy_20_summary.xlsx
-python3 make_xlsx.py ./fuse_busy_40 --out rfuse_busy_40_summary.xlsx
+python3 make_xlsx.py ./fuse --out fuse_summary.xlsx
+python3 make_xlsx.py ./fuse_busy_5 --out fuse_busy_5_summary.xlsx
+python3 make_xlsx.py ./fuse_busy_10 --out fuse_busy_10_summary.xlsx
+python3 make_xlsx.py ./fuse_busy_20 --out fuse_busy_20_summary.xlsx
+python3 make_xlsx.py ./fuse_busy_40 --out fuse_busy_40_summary.xlsx
 #python3 make_xlsx.py ./fuse_Ecore --out rfuse_Ecore_summary.xlsx
 
 python3 merge_xlsx.py . --out merged_summary.xlsx
