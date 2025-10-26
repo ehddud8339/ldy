ce7f248eadb939409e98872d10e267a9daefa789
ehddud8339 <ehddud8339@gmail.com>
2025-10-26 14:38:11 +0900

    update from local

--- rfuse/driver.sh
+++ rfuse/driver.sh
@@ -7,11 +7,11 @@ set -euo pipefail
 #python3 make_xlsx.py ./rfuse --out rfuse_summary.xlsx
 #python3 make_xlsx.py ./rfuse_rr --out rfuse_rr_summary.xlsx
 #python3 make_xlsx.py ./rfuse_thr --out rfuse_thr_summary.xlsx
-python3 make_xlsx.py ./rfuse --out rfuse_summary.xlsx
-python3 make_xlsx.py ./rfuse_busy_2 --out rfuse_busy_2_summary.xlsx
-python3 make_xlsx.py ./rfuse_busy_6 --out rfuse_busy_6_summary.xlsx
-python3 make_xlsx.py ./rfuse_busy_12 --out rfuse_busy_12_summary.xlsx
-python3 make_xlsx.py ./rfuse_busy_24 --out rfuse_busy_24_summary.xlsx
+python3 make_xlsx.py ./rfuse_busy --out rfuse_busy_summary.xlsx
+#python3 make_xlsx.py ./rfuse_busy_2 --out rfuse_busy_2_summary.xlsx
+#python3 make_xlsx.py ./rfuse_busy_6 --out rfuse_busy_6_summary.xlsx
+#python3 make_xlsx.py ./rfuse_busy_12 --out rfuse_busy_12_summary.xlsx
+#python3 make_xlsx.py ./rfuse_busy_24 --out rfuse_busy_24_summary.xlsx
 
 #python3 make_xlsx.py ./rfuse_Ecore --out rfuse_Ecore_summary.xlsx
 
